00f7d5515a00ee848d10e6b1b5130b72b3a681a7
richie1988 <richiessikaonga@gmail.com>
2023-10-11 14:49:23 +0200

    Fixed the conflicts in the main App

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './missions/missionsSlice';
+import rocketsReducer from './rockets/RocketsSlice';
 
 const store = configureStore({
   reducer: {
+    rockets: rocketsReducer,
     missions: missionsReducer,
   },
 });
