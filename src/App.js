08596b24a06be2d9ab86efa67a9556210301a632
rmiyoyo <miyoyoraphael@gmail.com>
2023-10-13 15:33:11 +0300

    Add dragons path

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Missions from './pages/Mission';
 import { fetchRockets } from './redux/rockets/RocketsSlice';
-import { fetchMissions } from './redux/missions/missionsSlice';
 import Rockets from './components/Rockets';
+import Dragon from './components/Dragon';
 import Profile from './pages/Profile';
 
 function App() {
@@ -14,7 +14,6 @@ function App() {
 
   useEffect(() => {
     dispatch(fetchRockets());
-    dispatch(fetchMissions());
   }, [dispatch]);
 
   return (
@@ -24,6 +23,7 @@ function App() {
         <Route path="/" element={<Rockets />} />
         <Route path="/rockets" element={<Rockets />} />
         <Route path="/missions" element={<Missions />} />
+        <Route path="/dragons" element={<Dragon />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
